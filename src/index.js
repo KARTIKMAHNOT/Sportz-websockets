e5bb18502844bb6d0c66f54d720003cb1e7cc7fe
Kartik Mahnot <kartikmahnot1@gmail.com>
2026-02-03 18:13:53 +0530

    REST endpoints for matches

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchRouter } from './router/matches.js';
 const app = express();
 const port = 8000
 
@@ -7,6 +8,7 @@ app.use(express.json())
 app.get('/',(req,res) => {
     res.send("Hello World");
 })
+app.use('/matches',matchRouter)
 app.listen(port,()=>{
     console.log("Server is running on port " + port);
 })
